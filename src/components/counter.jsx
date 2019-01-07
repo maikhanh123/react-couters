e4a3ec91cdf470b56d1c2648baedaa897c48e5cb
maikhanh <maikhanh123@gmail.com>
2019-01-07 09:21:44 +0700

    add prop of component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2"]
+    value: this.props.value
   };
 
   //   constructor(){
@@ -27,17 +26,17 @@ class Counter extends Component {
 
   handleIncrement = productId => {
     console.log(productId);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.value + 1 });
   };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
